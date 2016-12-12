dcf8ff2fab97dcbfd517096e603e7025680206c5
Eli Fatsi <eli.fatsi@viget.com>
2016-12-12 12:18:42 -0700

    Allow for customizable create_path

--- lib/bold/api_operations/create.rb
+++ lib/bold/api_operations/create.rb
@@ -2,7 +2,13 @@ module Bold
   module APIOperations
     module Create
       def create(params)
-        resource_from post(resource_path, body: params)
+        resource_from post(create_path, body: params)
+      end
+
+      private
+
+      def create_path
+        resource_path
       end
     end
   end
--- lib/bold/transfer.rb
+++ lib/bold/transfer.rb
@@ -3,5 +3,11 @@ module Bold
     def self.display_keys
       ["id", "status", "source_amount"]
     end
+
+    private
+
+    def self.create_path
+      resource_path + "/send"
+    end
   end
 end
